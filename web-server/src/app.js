a4c909904c06601de8ed6d1d51166c932d98ce3c
AnushkaM02 <anushka.mulik@gmail.com>
2022-08-11 15:39:56 +0530

    added geocode

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,23 +1,22 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-// console.log(__dirname);
-// .. to go up 1 directory
-// console.log(path.join(__dirname, "../public"));
 const app = express();
 
-// Define paths for express config
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
-const partialPath = path.join(__dirname, "../templates/partials");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
-// Set up handlebars engine and views location
+// Setup handlebars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
-hbs.registerPartials(partialPath);
+hbs.registerPartials(partialsPath);
 
-// Set up static directory to serve
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
@@ -36,51 +35,70 @@ app.get("/about", (req, res) => {
 
 app.get("/help", (req, res) => {
   res.render("help", {
-    helpText: "This is some helpful text",
+    helpText: "This is some helpful text.",
     title: "Help",
     name: "Anushka",
   });
 });
 
-// app.get("", (req, res) => {
-//   res.send("<h1>Weather</h1>");
-// });
-
-// app.get("/help", (req, res) => {
-//   res.send({
-//     name: "Andrew",
-//     age: 27,
-//   });
-// });
-
-// app.get("/about", (req, res) => {
-//   res.send("<h1>About</h1>");
-// });
-
 app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address!",
+    });
+  }
+
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
+      });
+    }
+  );
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+
+  console.log(req.query.search);
   res.send({
-    forecast: "It is 50 degrees",
-    location: "Philadelphia",
+    products: [],
   });
 });
 
 app.get("/help/*", (req, res) => {
   res.render("404", {
-    title: "404 help",
+    title: "404",
     name: "Anushka",
-    errorMessage: "Help article not found",
+    errorMessage: "Help article not found.",
   });
 });
 
-// * means match anything which has not been matched so far
 app.get("*", (req, res) => {
   res.render("404", {
     title: "404",
     name: "Anushka",
-    errorMessage: "Page not found",
+    errorMessage: "Page not found.",
   });
 });
 
 app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+  console.log("Server is up on port 3000.");
 });
