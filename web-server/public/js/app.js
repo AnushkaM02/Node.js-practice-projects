402cb270745917b173a66e1616863fb70d476b19
AnushkaM02 <anushka.mulik@gmail.com>
2022-08-15 15:41:27 +0530

    Setup app for heroku

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -22,20 +22,18 @@ weatherform.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading...";
   messageOne.textContent = "";
 
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          // console.log(data.error);
-          messageOne.textContent = data.error;
-        } else {
-          // console.log(data.location);
-          // console.log(data.forecast);
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        // console.log(data.error);
+        messageOne.textContent = data.error;
+      } else {
+        // console.log(data.location);
+        // console.log(data.forecast);
 
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
