04a85a9ac6615386a4a2668bb2057bec0a8be89e
AnushkaM02 <anushka.mulik@gmail.com>
2022-08-27 12:00:45 +0530

    task app mongodb and promises

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,22 +1,37 @@
-setTimeout(() => {
-  console.log("2s done");
-}, 2000);
+// setTimeout(() => {
+//   console.log("2s done");
+// }, 2000);
 
-const names = ["abc", "bd", "cferb"];
-const shortNames = names.filter((name) => {
-  return name.length <= 3;
-});
+// const names = ["abc", "bd", "cferb"];
+// const shortNames = names.filter((name) => {
+//   return name.length <= 3;
+// });
 
-const geocode = (address, callback) => {
+// const geocode = (address, callback) => {
+//   setTimeout(() => {
+//     const data = {
+//       latitude: 0,
+//       longitude: 0,
+//     };
+//     callback(data);
+//   }, 2000);
+// };
+
+// geocode("Philadelphia", (data) => {
+//   console.log(data);
+// });
+
+const doWorkCallback = (callback) => {
   setTimeout(() => {
-    const data = {
-      latitude: 0,
-      longitude: 0,
-    };
-    callback(data);
+    // callback("This is my error", undefined);
+    callback(undefined, [1, 4, 7]);
   }, 2000);
 };
 
-geocode("Philadelphia", (data) => {
-  console.log(data);
+doWorkCallback((error, result) => {
+  if (error) {
+    return console.log(error);
+  }
+
+  console.log(result);
 });
--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -7,10 +7,6 @@ const { MongoClient, ObjectID } = require("mongodb");
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 
-// const id = new ObjectID();
-// console.log(id);
-// console.log(id.getTimestamp());
-
 MongoClient.connect(
   connectionURL,
   { useNewUrlParser: true },
@@ -21,77 +17,178 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    // db.collection("users").insertOne(
-    //   {
-    //     name: "Anushka",
-    //     age: 20,
-    //   },
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert user");
-    //     }
-
-    //     console.log(result.acknowledged);
-    //   }
-    // );
-
-    // db.collection("users").insertOne(
-    //   {
-    //     _id: id,
-    //     name: "Serena",
-    //     age: 22,
-    //   },
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert user");
-    //     }
-
-    //     console.log(result.acknowledged);
-    //   }
-    // );
-
-    // db.collection("users").insertMany(
-    //   [
-    //     {
-    //       name: "Jen",
-    //       age: 28,
-    //     },
-    //     {
-    //       name: "Blair",
-    //       age: 23,
-    //     },
-    //   ],
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert documents");
-    //     }
-
-    //     console.log(result.acknowledged);
-    //   }
-    // );
-
-    // db.collection("tasks").insertMany(
-    //   [
-    //     {
-    //       description: "Clean the house",
-    //       completed: true,
-    //     },
-    //     {
-    //       description: "Renew inspection",
-    //       completed: false,
-    //     },
-    //     {
-    //       description: "Put plants",
-    //       completed: false,
-    //     },
-    //   ],
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert tasks");
-    //     }
-
-    //     console.log(result.acknowledged);
-    //   }
-    // );
+    db.collection("tasks")
+      .deleteOne({
+        description: "Clean the house",
+      })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 );
+
+// const id = new ObjectID();
+// console.log(id);
+// console.log(id.getTimestamp());
+
+// // In connect:
+// db.collection("users").insertOne(
+//   {
+//     name: "Anushka",
+//     age: 20,
+//   },
+//   (error, result) => {
+//     if (error) {
+//       return console.log("Unable to insert user");
+//     }
+
+//     console.log(result.acknowledged);
+//   }
+// );
+
+// db.collection("users").insertOne(
+//   {
+//     _id: id,
+//     name: "Serena",
+//     age: 22,
+//   },
+//   (error, result) => {
+//     if (error) {
+//       return console.log("Unable to insert user");
+//     }
+
+//     console.log(result.acknowledged);
+//   }
+// );
+
+// db.collection("users").insertMany(
+//   [
+//     {
+//       name: "Jen",
+//       age: 28,
+//     },
+//     {
+//       name: "Blair",
+//       age: 23,
+//     },
+//   ],
+//   (error, result) => {
+//     if (error) {
+//       return console.log("Unable to insert documents");
+//     }
+
+//     console.log(result.acknowledged);
+//   }
+// );
+
+// db.collection("tasks").insertMany(
+//   [
+//     {
+//       description: "Clean the house",
+//       completed: true,
+//     },
+//     {
+//       description: "Renew inspection",
+//       completed: false,
+//     },
+//     {
+//       description: "Put plants",
+//       completed: false,
+//     },
+//   ],
+//   (error, result) => {
+//     if (error) {
+//       return console.log("Unable to insert tasks");
+//     }
+
+//     console.log(result.acknowledged);
+//   }
+// );
+
+// db.collection("users").findOne({ name: "Jen" }, (error, user) => {
+//   if (error) {
+//     return console.log("Unable to fetch");
+//   }
+
+// db.collection("users").findOne(
+//   { _id: new ObjectID("630518385f56c24a6fa784f7") },
+//   (error, user) => {
+//     if (error) {
+//       return console.log("Unable to fetch");
+//     }
+
+//     console.log(user);
+//   }
+// );
+
+// db.collection("users")
+//   .find({ age: 20 })
+//   .toArray((error, users) => {
+//     console.log(users);
+//   });
+
+// db.collection("tasks").findOne(
+//   { _id: new ObjectID("6305153b3c86ed1fca73c489") },
+//   (error, user) => {
+//     console.log(user);
+//   }
+
+// db.collection("tasks")
+//   .find({ completed: false })
+//   .toArray((error, tasks) => {
+//     console.log(tasks);
+//   });
+
+//   db.collection("users")
+//     .updateOne(
+//       {
+//         _id: new ObjectID("6301efd7befb3725258c3c09"),
+//       },
+//       // {
+//       //   $set: {
+//       //     name: "Beck",
+//       //   },
+//       {
+//         $inc: {
+//           age: 1,
+//         },
+//       }
+//     )
+//     .then((result) => {
+//       console.log(result);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+
+// db.collection("tasks")
+//   .updateMany(
+//     {
+//       completed: false,
+//     },
+//     {
+//       $set: {
+//         completed: true,
+//       },
+//     }
+//   )
+//   .then((result) => {
+//     console.log(result.modifiedCount);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// db.collection("users")
+//   .deleteMany({
+//     age: 20,
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
